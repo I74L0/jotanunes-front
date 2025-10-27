c798f2cf270b02de29acf3c60315d2427faba1e3
Italo <4081360@banesecard.com>
2025-10-27 09:11:51 -0300

    Padronização de tema claro

--- src/App.js
+++ src/App.js
@@ -36,19 +36,10 @@ const ProtectedRoute = ({ children }) => {
 
 const App = () => {
   const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
-  const storedTheme = useSelector((state) => state.theme)
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.href.split('?')[1])
     const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
-    if (theme) {
-      setColorMode("light")
-    }
-
-    if (isColorModeSet()) {
-      return
-    }
-
     setColorMode("light")
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
